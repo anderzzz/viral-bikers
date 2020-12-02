db609f091ba2df3cbda9ed5c0675889114438808
anderzzz <ohrn.anders@gmail.com>
2020-12-02 14:30:00 +0100

    Tiny clean-up of blob maker script

--- dataset_creators/make_blob.py
+++ dataset_creators/make_blob.py
@@ -2,6 +2,8 @@
 
 '''
 import os
+import pandas as pd
+
 from dataset_creators import bikerawdata
 
 TPE_FILE_ROOT = '../bike_raw_data/taipei'
@@ -17,11 +19,15 @@ HEL_BLOB = '../data_blob/helsinki_bikeshare.csv'
 LON_BLOB = '../data_blob/london_bikeshare.csv'
 TOR_BLOB = '../data_blob/toronto_bikeshare.csv'
 
-for df in bikerawdata.parse('taipei', TPE_FILES):
-    df.to_csv(TPE_BLOB, mode='a', header=not os.path.exists(TPE_BLOB), index=False)
-for df in bikerawdata.parse('helsinki', HEL_FILES):
-    df.to_csv(HEL_BLOB, mode='a', header=not os.path.exists(HEL_BLOB), index=False)
-for df in bikerawdata.parse('taipei', LON_FILES):
-    df.to_csv(LON_BLOB, mode='a', header=not os.path.exists(LON_BLOB), index=False)
-for df in bikerawdata.parse('taipei', TOR_FILES):
-    df.to_csv(TOR_BLOB, mode='a', header=not os.path.exists(TOR_BLOB), index=False)
+def make_blob_by_city():
+    for df in bikerawdata.parse('taipei', TPE_FILES):
+        df.to_csv(TPE_BLOB, mode='a', header=not os.path.exists(TPE_BLOB), index=False)
+    for df in bikerawdata.parse('helsinki', HEL_FILES):
+        df.to_csv(HEL_BLOB, mode='a', header=not os.path.exists(HEL_BLOB), index=False)
+    for df in bikerawdata.parse('london', LON_FILES):
+        df.to_csv(LON_BLOB, mode='a', header=not os.path.exists(LON_BLOB), index=False)
+    for df in bikerawdata.parse('toronto', TOR_FILES):
+        df.to_csv(TOR_BLOB, mode='a', header=not os.path.exists(TOR_BLOB), index=False)
+
+if __name__ == '__main__':
+    make_blob_by_city()
